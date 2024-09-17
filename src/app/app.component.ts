86c76d2f2656495665f0fb1ac2af9825a3c2a4ee
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-09-17 16:23:27 -0600

    Permite agregar el componente de home y labs. También se configuro el redireccionamiento de las partes del proyecto para que las rutas sepan el componente a renderizar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  welcome = 'Hola!';
-  tasks = [
-    'Instalar el Angular CLI',
-    'Crear proyecto',
-    'Crear componentes'
-  ]
+  title = 'todo-app';
 }
