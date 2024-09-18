fa97a6ac499fc4a4fafcf91feefb5b0f5e32effa
Jorge Chan <jorgeluis.chanchan@gmail.com>
2024-09-18 15:06:55 -0600

    Permite agregar ejemplo del uso del event binding keydown y keydown para combinacion de teclas.

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -34,4 +34,9 @@ export class LabsComponent {
   changeHandler(event: Event) {
     console.log(event);
   }
+
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log(input.value);
+  }
 }
